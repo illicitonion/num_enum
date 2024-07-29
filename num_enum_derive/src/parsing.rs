b006342ebdacf00e0388cf82bc97dca638a50b92
Campbell He <campbell.he@icloud.com>
2024-07-29 20:06:52 +0800

    fix: filter catch_all variant in EnumInfo methods (#150)
    This commit filters out catch_all variant in the `variant_idents` and
    `variant_expressions` of `EnumInfo`. This allows the catch_all variant
    to be in the middle of enum.
    
    A test of catch_all variant in the middle of enum is also added.
    
    Fix #149

--- num_enum_derive/src/parsing.rs
+++ num_enum_derive/src/parsing.rs
@@ -61,6 +61,7 @@ impl EnumInfo {
     pub(crate) fn variant_idents(&self) -> Vec<Ident> {
         self.variants
             .iter()
+            .filter(|variant| !variant.is_catch_all)
             .map(|variant| variant.ident.clone())
             .collect()
     }
@@ -81,6 +82,7 @@ impl EnumInfo {
     pub(crate) fn variant_expressions(&self) -> Vec<Vec<Expr>> {
         self.variants
             .iter()
+            .filter(|variant| !variant.is_catch_all)
             .map(|variant| variant.all_values().cloned().collect())
             .collect()
     }
